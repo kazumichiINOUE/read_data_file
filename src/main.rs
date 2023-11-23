1df4e0f8e500486ec562180ffa34e4cafb469deb
KazumichiINOUE <k.inoue@oyama-ct.ac.jp>
2023-11-23 13:17:13 +0900

    feat: first commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+fn main() {
+    // データファイルのオープン
+    let log_path = "log_data";
+    let file = File::open(Path::new(&log_path)).expect("ログファイルのオープンに失敗しました");
+    let reader = BufReader::new(file);
+
+    // 読み込んだデータを格納するベクトル
+    let mut points_data = Vec::<(f64, f64, f64)>::new();
+
+    // ファイルからデータを読み込み，ベクトルへ格納
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            // 行を空白で分割
+            let parts: Vec<&str> = line.split_whitespace().collect();
+
+            if parts.len() == 7 {   // 7はlod_dataの列数
+                let x: f64 = parts[1].parse().expect("Xのパースに失敗しました");
+                let y: f64 = parts[2].parse().expect("Yのパースに失敗しました");
+                let a: f64 = parts[3].parse().expect("Aのパースに失敗しました");
+                points_data.push((x, y, a));
+            }
+        }
+    }
+
+    for data in points_data {
+        println!("x:{} y:{} a:{}", data.0, data.1, data.2);
+    }
+}
+
+
